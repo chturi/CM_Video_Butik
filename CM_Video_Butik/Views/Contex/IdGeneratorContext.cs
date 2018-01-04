74048ac631fed8cff3e03c074aa71be868e67048
Christopher Muturi <Muturi@kth.se>
2018-01-04 11:02:56 +0100

    revert

---
+++ CM_Video_Butik/Views/Contex/IdGeneratorContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CM_Video_Butik.Views.Contex
+{
+    public class IdGeneratorContext
+    {
+    }
+}
